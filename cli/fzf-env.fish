3932010aa2a5d8ccd650b158d23fe78fa45d723a
Dmitry Makarov <makarovdmv@gmail.com>
2026-04-09 22:55:39 +0300

    [modern-cli] Fix fzf-env function

--- cli/fzf-env.fish
+++ cli/fzf-env.fish
@@ -1,11 +1,13 @@
 function fzf-env --description "Search environment variables with fzf"
     set -f --export SHELL (command --search fish)
-    set -f set_show_output (set --show | psub)
+    set -f tmpfile (mktemp)
+    set --show >$tmpfile
     set -f all_variable_names (string match --invert history (set --names))
     printf '%s\n' $all_variable_names | \
         fzf --prompt="Variables> " \
-            --preview="_fzf_extract_var_info {} $set_show_output" \
+            --preview="_fzf_extract_var_info {} $tmpfile" \
             --preview-window="wrap" \
             --multi \
             $fzf_variables_opts
+    rm -f $tmpfile
 end
