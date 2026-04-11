e164a7cd1587f5a0fe82d1350cd7b71a28f9baf2
Dmitry Makarov <makarovdmv@gmail.com>
2026-04-11 13:24:49 +0300

    [modern-cli] Adjust fzf style to less borders

--- cli/fzf-aliases.bash
+++ cli/fzf-aliases.bash
@@ -5,16 +5,16 @@ fzf-fd() {
     ll_cmd=$(alias ll 2>/dev/null | sed -E "s/^alias ll='(.*)'/\1/")
 
     fd --color=always --hidden --follow -E .git -E .env -E .venv -E .cache | \
-        fzf --prompt="Directory> " --preview="bash -c 'if [ -d \"\$0\" ]; then ${ll_cmd:-ls} --color=always \"\$0\"; else bat --style=numbers --color=always \"\$0\" 2>/dev/null || cat \"\$0\"; fi' {}";
+        fzf --prompt="Directory> " --preview="bash -c 'if [ -d \"\$0\" ]; then ${ll_cmd:-ls} --color=always \"\$0\"; else bat --color=always \"\$0\" 2>/dev/null || cat \"\$0\"; fi' {}";
 }
 
 fzf-ps() {
     ps -eo pid,ppid,user,%cpu,%mem,start,command | \
-        fzf --prompt="Processes> " --header-lines=1 --preview="ps -o pid,ppid,user,%cpu,rss,start,command -p {1} 2>/dev/null || echo 'Process {1} has exited.'" --preview-window="bottom:4:wrap";
+        fzf --prompt="Processes> " --header-lines=1 --preview="ps -o pid,ppid,user,%cpu,rss,start,command -p {1} 2>/dev/null || echo 'Process {1} has exited.'" --preview-window="bottom:4:wrap" --preview-border="top";
 }
 
 fzf-history() {
-    history | fzf --tac --scheme=history --prompt="History> " --preview-window="bottom:3:wrap";
+    history | fzf --tac --scheme=history --prompt="History> " --preview-window="bottom:3:wrap" --preview-border="top";
 }
 
 fzf-env() {
