1e751616e601f5b024a931c7f05d87d5feab8a61
Dmitry Makarov <makarovdmv@gmail.com>
2026-08-08 20:05:21 +0300

    [prompt] Add keybindings for fish

---
+++ prompt/fish_user_key_bindings.fish
@@ -0,0 +1,6 @@
+function fish_user_key_bindings
+    bind ctrl-right         forward-word
+    bind ctrl-left          backward-word
+    bind ctrl-delete        kill-word
+    bind ctrl-backspace     backward-kill-word
+end
